f019de10b408b00b59568c4bd38c49c7f6df7348
Ziddan <ziddanalif2@gmail.com>
2021-10-18 17:40:03 +0700

    Adding variables

--- index.php
+++ index.php
@@ -10,6 +10,13 @@ define ("alamat","Jalan Cibolang Sukabumi");
 
 echo "Saya kuliah di : " .kampus. "<br>";
 echo "Jurusan yang saya ambil : " .jurusan. "<br>";
-echo "Kampusku beralamat di : " .alamat. "<br>";
+echo "Kampusku beralamat di : " .alamat. "<br><br>";
+
+#variable
+$buku = "Programing PHP";
+$penulis = "Joko Suntoro";
+
+echo "variabel <b>buku</b> mempunyai nilai : <b>" .$buku. "</b><br>";
+echo "variabel <b>penulis</b> mempunyai nilai : <b>" .$penulis. "</b><br><br>";
 
 ?>
