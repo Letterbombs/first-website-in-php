07931d46116b99c197e87bef890e1ba7f12153e0
Ziddan <ziddanalif2@gmail.com>
2021-10-18 17:26:30 +0700

    Adding konstanta

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
-echo "Hello World";
+#hello world code
+echo "Hello World <br><br>";
 
+#konstanta
+define ("kampus","Universitas Nusa Putra");
+define ("jurusan","Teknik Informatika");
+define ("alamat","Jalan Cibolang Sukabumi");
+
+echo "Saya kuliah di : " .kampus. "<br>";
+echo "Jurusan yang saya ambil : " .jurusan. "<br>";
+echo "Kampusku beralamat di : " .alamat. "<br>";
 
 ?>
